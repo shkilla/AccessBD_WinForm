463ff778caec538c23363faf2f9475a2d180d843
Oleg Parkhomenko <51717851+shkilla@users.noreply.github.com>
2022-11-27 23:34:40 +0300

    Add files via upload

---
+++ AccessDataBase/AccessDataBaseDemo/AccessForm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace AccessDataBase
+{
+    partial class AccessForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.действияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.обПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainDataGridBox = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GroupBoxAction = new System.Windows.Forms.GroupBox();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.button_update = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_download = new System.Windows.Forms.Button();
+            this.MenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainDataGridBox)).BeginInit();
+            this.GroupBoxAction.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.действияToolStripMenuItem,
+            this.обПрограммеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(671, 24);
+            this.MenuStrip.TabIndex = 7;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // действияToolStripMenuItem
+            // 
+            this.действияToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьToolStripMenuItem,
+            this.добавитьToolStripMenuItem});
+            this.действияToolStripMenuItem.Name = "действияToolStripMenuItem";
+            this.действияToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.действияToolStripMenuItem.Text = "Действия";
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьToolStripMenuItem.Text = "Об программе";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // добавитьToolStripMenuItem
+            // 
+            this.добавитьToolStripMenuItem.Name = "добавитьToolStripMenuItem";
+            this.добавитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.добавитьToolStripMenuItem.Text = "Выход";
+            this.добавитьToolStripMenuItem.Click += new System.EventHandler(this.добавитьToolStripMenuItem_Click);
+            // 
+            // обПрограммеToolStripMenuItem
+            // 
+            this.обПрограммеToolStripMenuItem.Name = "обПрограммеToolStripMenuItem";
+            this.обПрограммеToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.обПрограммеToolStripMenuItem.Text = "Об программе";
+            this.обПрограммеToolStripMenuItem.Click += new System.EventHandler(this.обПрограммеToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // MainDataGridBox
+            // 
+            this.MainDataGridBox.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MainDataGridBox.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.name,
+            this.cost,
+            this.quantity});
+            this.MainDataGridBox.Location = new System.Drawing.Point(12, 27);
+            this.MainDataGridBox.Name = "MainDataGridBox";
+            this.MainDataGridBox.Size = new System.Drawing.Size(446, 304);
+            this.MainDataGridBox.TabIndex = 8;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "ID";
+            this.Id.Name = "Id";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Имя";
+            this.name.Name = "name";
+            // 
+            // cost
+            // 
+            this.cost.HeaderText = "Стаж";
+            this.cost.Name = "cost";
+            // 
+            // quantity
+            // 
+            this.quantity.HeaderText = "Зарплата";
+            this.quantity.Name = "quantity";
+            // 
+            // GroupBoxAction
+            // 
+            this.GroupBoxAction.Controls.Add(this.button_delete);
+            this.GroupBoxAction.Controls.Add(this.button_update);
+            this.GroupBoxAction.Controls.Add(this.button_add);
+            this.GroupBoxAction.Controls.Add(this.button_download);
+            this.GroupBoxAction.Location = new System.Drawing.Point(464, 74);
+            this.GroupBoxAction.Name = "GroupBoxAction";
+            this.GroupBoxAction.Size = new System.Drawing.Size(195, 212);
+            this.GroupBoxAction.TabIndex = 9;
+            this.GroupBoxAction.TabStop = false;
+            this.GroupBoxAction.Text = "Действия";
+            // 
+            // button_delete
+            // 
+            this.button_delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_delete.Location = new System.Drawing.Point(6, 154);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(183, 35);
+            this.button_delete.TabIndex = 3;
+            this.button_delete.Text = "Удалить";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // button_update
+            // 
+            this.button_update.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_update.Location = new System.Drawing.Point(6, 113);
+            this.button_update.Name = "button_update";
+            this.button_update.Size = new System.Drawing.Size(183, 35);
+            this.button_update.TabIndex = 2;
+            this.button_update.Text = "Обновить";
+            this.button_update.UseVisualStyleBackColor = true;
+            this.button_update.Click += new System.EventHandler(this.button_update_Click);
+            // 
+            // button_add
+            // 
+            this.button_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_add.Location = new System.Drawing.Point(6, 72);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(183, 35);
+            this.button_add.TabIndex = 1;
+            this.button_add.Text = "Добавить";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_download
+            // 
+            this.button_download.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_download.Location = new System.Drawing.Point(6, 31);
+            this.button_download.Name = "button_download";
+            this.button_download.Size = new System.Drawing.Size(183, 35);
+            this.button_download.TabIndex = 0;
+            this.button_download.Text = "Загрузить";
+            this.button_download.UseVisualStyleBackColor = true;
+            this.button_download.Click += new System.EventHandler(this.button_download_Click);
+            // 
+            // AccessForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 343);
+            this.Controls.Add(this.GroupBoxAction);
+            this.Controls.Add(this.MainDataGridBox);
+            this.Controls.Add(this.MenuStrip);
+            this.MainMenuStrip = this.MenuStrip;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(687, 382);
+            this.MinimumSize = new System.Drawing.Size(687, 382);
+            this.Name = "AccessForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Подключение базы данных Access";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainDataGridBox)).EndInit();
+            this.GroupBoxAction.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem действияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem обПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.DataGridView MainDataGridBox;
+        private System.Windows.Forms.GroupBox GroupBoxAction;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Button button_update;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_download;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cost;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantity;
+    }
+}
+
---
+++ AccessDataBase/AccessDataBaseDemo/AccessForm.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccessDataBase
+{
+    public partial class AccessForm : Form
+    {
+        // конструктор класса формы
+        public AccessForm()
+        {
+            InitializeComponent();
+        }
+
+        // обработчик события закрытия формы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // отключаемся от базы данных
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Подключаем базу данных
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Workers.mdb";  //строка соеденения
+            //ссылка на экземпляр класса OleDbConnection для соединения с Базой данных
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);                  //создаем соеденение
+            OleDbDataReader dbReader;
+            try
+            {
+                // Выполянем запрос к БД
+                dbConnection.Open();                                            // открываем соеденение
+                string query = "SELECT * FROM Worker";                          // строка запроса
+                OleDbCommand dbCommand = new OleDbCommand(query, dbConnection); // команда
+                dbReader = dbCommand.ExecuteReader();                           // считываем данные
+
+                // Проверяем данные
+                if (dbReader.HasRows == false)
+                {
+                    MessageBox.Show("Данные не найдены!", "Ошибка!");
+                }
+                else
+                {
+                    // Чтение данных
+                    while (dbReader.Read())
+                    {
+                        // Выводим данные
+                        MainDataGridBox.Rows.Add(dbReader["id"], dbReader["Name"], dbReader["Cost"], dbReader["Quantity"]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Сообщение: {ex.Message}", "Вызвано исключение!");
+            }
+            finally
+            {
+                // Закрываем соеденение с БД
+                dbConnection.Close();
+            }
+        }
+
+        private void обПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа подключения Базы данных, также её просмотр и редактирование.", "Об программе");
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_download_Click(object sender, EventArgs e)
+        {
+            // Подключаем базу данных
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Workers.mdb";  //строка соеденения
+            //ссылка на экземпляр класса OleDbConnection для соединения с Базой данных
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);                  //создаем соеденение
+            OleDbDataReader dbReader;
+            try
+            {
+                // Выполянем запрос к БД
+                dbConnection.Open();                                            // открываем соеденение
+                string query = "SELECT * FROM Worker";                          // строка запроса
+                OleDbCommand dbCommand = new OleDbCommand(query, dbConnection); // команда
+                dbReader = dbCommand.ExecuteReader();                           // считываем данные
+
+                // Проверяем данные
+                if (dbReader.HasRows == false)
+                {
+                    MessageBox.Show("Данные не найдены!", "Ошибка!");
+                }
+                else
+                {
+                    // Чтение данных
+                    while (dbReader.Read())
+                    {
+                        // Выводим данные
+                        MainDataGridBox.Rows.Add(dbReader["id"], dbReader["Name"], dbReader["Cost"], dbReader["Quantity"]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Сообщение: {ex.Message}", "Вызвано исключение!");
+            }
+            finally
+            {
+                // Закрываем соеденение с БД
+                dbConnection.Close();
+            }
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            //проверка количества выбранных строк
+            if (MainDataGridBox.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!", "Внимание!");
+                return;
+            }
+
+            //записываем выбранную строку
+            int index = MainDataGridBox.SelectedRows[0].Index;
+
+            //проверка данных в таблице
+            if (MainDataGridBox.Rows[index].Cells[0].Value == null ||
+                MainDataGridBox.Rows[index].Cells[1].Value == null ||
+                MainDataGridBox.Rows[index].Cells[2].Value == null ||
+                MainDataGridBox.Rows[index].Cells[3].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!", "Внимание!");
+                return;
+            }
+
+            // считаем данные
+            string id = MainDataGridBox.Rows[index].Cells[0].Value.ToString();
+            string name = MainDataGridBox.Rows[index].Cells[1].Value.ToString();
+            string cost = MainDataGridBox.Rows[index].Cells[2].Value.ToString();
+            string quantity = MainDataGridBox.Rows[index].Cells[3].Value.ToString();
+
+            // создаем соеденение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Workers.mdb";
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);
+
+            try
+            {
+                // Выполянем запрос к БД
+                dbConnection.Open();                                                                                     //открываем соеденение
+                string query = "INSERT INTO Worker VALUES (" + id + ", '" + name + "', " + cost + ", " + quantity + ")"; //строка запроса
+                OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);                                          //команда
+
+                // Выполняем запрос
+                if (dbCommand.ExecuteNonQuery() != 1)
+                    MessageBox.Show("Ошибка выполнения запроса!", "Ошибка!");
+                else
+                    MessageBox.Show("Данные добавлены!", "Внимание!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Сообщение: {ex.Message}", "Вызвано исключение!");
+            }
+            finally
+            {
+                // Закрываем соеденение с БД
+                dbConnection.Close();
+            }
+        }
+
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            // Проверим количество выбранных строк
+            if (MainDataGridBox.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!", "Внимание!");
+                return;
+            }
+
+            // Запомним выбранную строку
+            int index = MainDataGridBox.SelectedRows[0].Index;
+
+            // Проверим данные в таблицы
+            if (MainDataGridBox.Rows[index].Cells[0].Value == null ||
+                MainDataGridBox.Rows[index].Cells[1].Value == null ||
+                MainDataGridBox.Rows[index].Cells[2].Value == null ||
+                MainDataGridBox.Rows[index].Cells[3].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!", "Внимание!");
+                return;
+            }
+
+            // Считаем данные
+            string id = MainDataGridBox.Rows[index].Cells[0].Value.ToString();
+            string name = MainDataGridBox.Rows[index].Cells[1].Value.ToString();
+            string cost = MainDataGridBox.Rows[index].Cells[2].Value.ToString();
+            string quantity = MainDataGridBox.Rows[index].Cells[3].Value.ToString();
+
+            // Создаем соеденение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Workers.mdb"; //строка соеденения
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);                 //создаем соеденение
+
+            try
+            {
+                // Выполянем запрос к БД
+                dbConnection.Open();//открываем соеденение
+                string query = "UPDATE Worker SET Name = '" + name + "',Cost=" + cost + ",Quantity=" + quantity + " WHERE id = " + id;//строка запроса
+                OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);                                                       //команда
+
+                //Выполняем запрос
+                if (dbCommand.ExecuteNonQuery() != 1)
+                    MessageBox.Show("Ошибка выполнения запроса!", "Ошибка!");
+                else
+                {
+                    MessageBox.Show("Данные изменены!", "Внимание!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Сообщение: {ex.Message}", "Вызвано исключение!");
+            }
+            finally
+            {
+                // Закрываем соеденение с БД
+                dbConnection.Close();
+            }
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            //Проверим количество выбранных строк
+            if (MainDataGridBox.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!", "Внимание!");
+                return;
+            }
+
+            //Запомним выбранную строку
+            int index = MainDataGridBox.SelectedRows[0].Index;
+
+            // Проверим данные в таблицы
+            if (MainDataGridBox.Rows[index].Cells[0].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!", "Внимание!");
+                return;
+            }
+
+            // Считаем данные
+            string id = MainDataGridBox.Rows[index].Cells[0].Value.ToString();
+
+            // Создаем соеденение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Workers.mdb";  // строка соеденения
+            OleDbConnection dbConnection = new OleDbConnection(connectionString);                   // создаем соеденение
+
+            try
+            {
+                // Выполянем запрос к БД
+                dbConnection.Open();                                            // открываем соеденение
+                string query = "DELETE FROM Worker WHERE id = " + id;       // строка запроса
+                OleDbCommand dbCommand = new OleDbCommand(query, dbConnection); // команда
+
+                // Выполняем запрос
+                if (dbCommand.ExecuteNonQuery() != 1)
+                    MessageBox.Show("Ошибка выполнения запроса!", "Ошибка!");
+                else
+                {
+                    MessageBox.Show("Данные удалены!", "Внимание!");
+                    // Удаляем данные из таблицы в форме
+                    MainDataGridBox.Rows.RemoveAt(index);
+                }
+            }
+            catch (Exception ex)
+            {
+                //throw ex;
+                MessageBox.Show($"Сообщение: {ex.Message}", "Вызвано исключение!");
+            }
+            finally
+            {
+                // Закрываем соеденение с БД
+                dbConnection.Close();
+            }
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа подключения Базы данных, также её просмотр и редактирование.", "Об программе");
+
+        }
+
+        private void добавитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
